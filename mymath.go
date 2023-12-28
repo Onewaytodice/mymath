4c86a57cf7d2e6ca19be158e742d38f0bcd95250
Ivan Guziev <zik4040@gmail.com>
2023-12-28 20:14:15 +0300

    add some func in package "mymath"

--- mymath.go
+++ mymath.go
@@ -21,3 +21,11 @@ func Abs(x float64) float64 {
 func Exp(x float64) float64 {
 	return math.Exp(x)
 }
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
