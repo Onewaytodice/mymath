4de4890a83e9d32e48df2174c2ea8a74b228c46c
Ivan Guziev <zik4040@gmail.com>
2023-12-28 19:16:21 +0300

    add package "mymath" module "test"

---
+++ mymath.go
@@ -0,0 +1,23 @@
+package mymath
+
+import "math"
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Exp(x float64) float64 {
+	return math.Exp(x)
+}
